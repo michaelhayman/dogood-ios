8b6d2810b3b3329c2b4d7f54383bd94e1bd3c73b
Michael Hayman <michael.hayman@zoocasa.com>
2013-10-11 13:22:57 -0400

    Reference external class for user overview & store good ID.

--- DoGood/DGGoodListViewController.h
+++ DoGood/DGGoodListViewController.h
@@ -1,14 +1,16 @@
 #import "RootViewController.h"
 @class DGCategory;
+@class UserOverview;
 
 @interface DGGoodListViewController : RootViewController <UITableViewDelegate, UITableViewDataSource> {
     IBOutlet UITableView *tableView;
     IBOutlet UIView *headerView;
     NSArray *goods;
-
+    UserOverview *userView;
 }
 
 @property (nonatomic, retain) DGCategory *category;
+@property (nonatomic, retain) NSNumber *goodID;
 
 - (void)showWelcome;
 
