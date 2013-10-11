77fbe2669c17ff80ff73d61b49ce90427970f956
Michael Hayman <michael.hayman@zoocasa.com>
2013-10-11 13:20:03 -0400

    Remove unused methods & variables.

--- DoGood/DGUserSettingsViewController.h
+++ DoGood/DGUserSettingsViewController.h
@@ -1,12 +1,11 @@
 @class DGUserInvitesViewController;
 @class DGPhotoPickerViewController;
-@interface DGUserSettingsViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIActionSheetDelegate, UIAlertViewDelegate> {
+@interface DGUserSettingsViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIActionSheetDelegate, UIAlertViewDelegate> {
     UITapGestureRecognizer *dismissTap;
     
     __weak IBOutlet UIImageView *avatar;
     __weak IBOutlet UIImageView *avatarOverlay;
     UIImage *imageToUpload;
-    UIActionSheet *photoSheet;
     DGUserInvitesViewController *invites;
     DGPhotoPickerViewController *photos;
 
