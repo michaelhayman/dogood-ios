422a2374eb727f0608f3a05d543531216f2bfe9b
Michael Hayman <michael.hayman@zoocasa.com>
2013-10-15 17:36:59 -0400

    Reorganize constants file.

--- DoGood/constants.h
+++ DoGood/constants.h
@@ -1,44 +1,22 @@
-#pragma mark - Convenience
+#pragma mark - Paths -----
+
+#define DEBUG 1
+#if DEBUG
+    #define JSON_API_HOST_ADDRESS @"http://0.0.0.0:3002/"
+#else
+    #define JSON_API_HOST_ADDRESS @"http://dogood-app.herokuapp.com/"
+#endif
+
+#pragma mark - Convenience -----
 
 #define iPad    UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
 
-#pragma mark - Connectivity
-
-#define checkConnectivity NO
-#define checkAPI NO
-
-#pragma mark - Third parties
-
-#define FOURSQUARE_API_URL       @"https://api.foursquare.com"
-#define FOURSQUARE_CLIENT_ID     @"EWRCKLKQ4O2LVVYK1ADLNXHTBS3MTYY1JMNPNJCM3SZ1ATII"
-#define FOURSQUARE_CLIENT_SECRET @"VZGH0QRJFF4AOU3WTXON0XZZQJ3YKMYLEUQ3ZRCQ0HZBDVTP"
-
-#define TWITTER_CONSUMER_KEY @"yT577ApRtZw51q4NPMPPOQ"
-#define TWITTER_CONSUMER_SECRET @"3neq3XqN5fO3obqwZoajavGFCUrC42ZfbrLXy5sCv8"
-
-// Paths
-// #define JSON_API_HOST_ADDRESS @"http://192.168.1.3:3002/"
-#define JSON_API_HOST_ADDRESS @"http://0.0.0.0:3002/"
-
-#pragma mark - User
-
-// Paths
-#define user_session_path @"/users/sign_in"
-#define user_end_session_path @"/users/sign_out"
-#define user_registration_path @"/users"
-#define user_password_path @"/users/password"
-#define user_update_path @"/users/update_profile"
-#define user_remove_avatar_path @"/users/remove_avatar"
+#pragma mark - Styles -----
 
 // Styles
 #define LINK_COLOUR [UIColor colorWithRed:5.0/255.0 green:171.0/255.0 blue:117.0/255.0 alpha:1.0]
-// #define NEUTRAL_BACKGROUND_COLOUR [UIColor colorWithRed:224.0/255.0 green:228.0/255.0 blue:204.0/255.0 alpha:1.0]
-// #define NEUTRAL_BACKGROUND_COLOUR [UIColor colorWithRed:230.0/255.0 green:223.0/255.0 blue:190.0/255.0 alpha:1.0]
 // nice neutral
 #define NEUTRAL_BACKGROUND_COLOUR [UIColor colorWithRed:237.0/255.0 green:234.0/255.0 blue:227.0/255.0 alpha:1.0]
-// #define NEUTRAL_BACKGROUND_COLOUR [UIColor colorWithRed:197.0/255.0 green:240.0/255.0 blue:249.0/255.0 alpha:1.0]
-// poo green
-//#define BUTTON_COLOR [UIColor colorWithRed:173.0/255.0 green:183.0/255.0 blue:36.0/255.0 alpha:1.0]
 // brown
 #define BUTTON_COLOR [UIColor colorWithRed:122.0/255.0 green:106.0/255.0 blue:83.0/255.0 alpha:1.0]
 #define GRAYED_OUT [UIColor colorWithRed:200.0/255.0 green:200.0/255.0 blue:200.0/255.0 alpha:0.8]
@@ -59,6 +37,16 @@
 
 #define MENU_FONT_COLOR [UIColor blackColor]
 
+#pragma mark - User -----
+
+// Paths
+#define user_session_path @"/users/sign_in"
+#define user_end_session_path @"/users/sign_out"
+#define user_registration_path @"/users"
+#define user_password_path @"/users/password"
+#define user_update_path @"/users/update_profile"
+#define user_remove_avatar_path @"/users/remove_avatar"
+
 // RKPassword
 #define kDoGoodService @"DoGood"
 #define kDoGoodAccount @"DoGood"
@@ -76,6 +64,8 @@ extern NSString* const kDGUserCurrentUserContactable;
 extern NSString* const kDGUserCurrentUserAvatar;
 extern NSString* const kDGUserCurrentUserTwitterID;
 extern NSString* const kDGUserCurrentUserFacebookID;
+
+#pragma mark - Notifications -----
 
 // Notifications
 extern NSString *const DGUserDidSignOutNotification;
@@ -151,5 +141,16 @@ extern NSString* const DGSearchTextFieldDidBeginEditing;
 extern NSString* const DGSearchTextFieldDidEndEditing;
 extern NSString* const DGUserDidStartBrowsingSearchTable;
 
-// convenience methods
+#pragma mark - Convenience -----
+
 #define NSNullIfNil(v) (v ? v : [NSNull null])
+
+#pragma mark - Third parties -----
+
+#define FOURSQUARE_API_URL       @"https://api.foursquare.com"
+#define FOURSQUARE_CLIENT_ID     @"EWRCKLKQ4O2LVVYK1ADLNXHTBS3MTYY1JMNPNJCM3SZ1ATII"
+#define FOURSQUARE_CLIENT_SECRET @"VZGH0QRJFF4AOU3WTXON0XZZQJ3YKMYLEUQ3ZRCQ0HZBDVTP"
+
+#define TWITTER_CONSUMER_KEY @"yT577ApRtZw51q4NPMPPOQ"
+#define TWITTER_CONSUMER_SECRET @"3neq3XqN5fO3obqwZoajavGFCUrC42ZfbrLXy5sCv8"
+
