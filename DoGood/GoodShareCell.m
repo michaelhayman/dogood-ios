0bcbfeea82bddaa6a8622a30f1f4f860c8abddfc
Michael Hayman <michael.hayman@zoocasa.com>
2013-10-20 12:53:28 -0400

    Change tint colour of switches.

--- DoGood/GoodShareCell.m
+++ DoGood/GoodShareCell.m
@@ -7,6 +7,7 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     self.share.on = NO;
+    self.share.onTintColor = COLOUR_GREEN;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
