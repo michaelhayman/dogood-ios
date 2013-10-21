7c458e4c012a668710b5995c33b739d2dc7457c3
Michael Hayman <michael.hayman@zoocasa.com>
2013-10-21 13:06:50 -0400

    Add to test suite.

---
+++ DoGoodTests/DGUserTests.m
@@ -0,0 +1,103 @@
+#import <XCTest/XCTest.h>
+#import "DGUser.h"
+
+@interface DGUserTests : XCTestCase {
+    DGUser *newUser;
+}
+
+@end
+
+// static DGUser* existingUser = nil;
+
+@implementation DGUserTests
+
+/*
++ (void)setUp {
+    [super setUp];
+    existingUser = [DGUser currentUser];
+    NSLog(@"setup %@", existingUser.email);
+}
+
++ (void)tearDown {
+    [super tearDown];
+    [DGUser setCurrentUser:existingUser];
+    NSLog(@"tear down %@", [DGUser currentUser].email);
+}
+*/
+
+- (void)setUp {
+    [super setUp];
+    newUser = [[DGUser alloc] init];
+    newUser.full_name = @"Michael Hayman";
+    newUser.email = @"mike@springbox.ca";
+    newUser.password = @"sn00shie";
+    newUser.biography = @"Makes apps";
+    newUser.location = @"Toronto";
+    newUser.phone = @"416 365-7308";
+    newUser.twitter_id = @"230840283408234";
+    newUser.facebook_id = @"108108081081";
+    newUser.points = [NSNumber numberWithInt:10000];
+    newUser.userID = [NSNumber numberWithInt:10808];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testUserExists {
+    DGUser *anotherUser = [[DGUser alloc] init];
+    XCTAssertNotNil(anotherUser, @"should be able to create a User instance");
+}
+
+- (void)testUserIsNilByDefault {
+    DGUser *anotherUser = [[DGUser alloc] init];
+    XCTAssertNil(anotherUser.full_name, @"Name should be nil on startup.");
+}
+
+- (void)testUserHasAFullName {
+    XCTAssertEqual(@"Michael Hayman", newUser.full_name, @"Attributes should get set");
+}
+
+- (void)testUserHasAnEmailAddress {
+    XCTAssertEqual(@"mike@springbox.ca", newUser.email, @"Set email");
+}
+
+- (void)testUserHasABiography {
+    XCTAssertEqual(@"Makes apps", newUser.biography, @"Set biography");
+}
+
+- (void)testUserHasALocation {
+    XCTAssertEqual(@"Toronto", newUser.location, @"User must have a location");
+}
+
+- (void)testUserHasATwitterID {
+    XCTAssertEqualObjects(@"230840283408234", newUser.twitter_id, @"User must have a twitter ID");
+}
+
+- (void)testUserHasAFacebookID {
+    XCTAssertEqualObjects(@"108108081081", newUser.facebook_id, @"User must have a facebook ID");
+}
+
+- (void)testUserHasAPhoneNumber {
+    XCTAssertEqualObjects(@"416 365-7308", newUser.phone, @"User must have a phone number");
+}
+
+- (void)testUserHasPoints {
+    XCTAssertEqualObjects([NSNumber numberWithInt:10000], newUser.points, @"User must have points");
+}
+
+- (void)testUserHasAnID {
+    XCTAssertEqualObjects([NSNumber numberWithInt:10808], newUser.userID, @"User must have an ID");
+}
+
+- (void)testUserSignIn {
+    XCTAssertTrue([newUser isSignedIn], @"user should be signed in");
+}
+
+// can't do this since it screws with the actual app testing
+- (void)testUserSignOut {
+    [newUser signOutWithMessage:NO];
+    XCTAssertFalse([newUser isSignedIn], @"user should be signed out");
+}
+
+@end
--- DoGoodTests/DoGoodTests.m
+++ DoGoodTests/DoGoodTests.m
@@ -1,34 +1,57 @@
-//
-//  DoGoodTests.m
-//  DoGoodTests
-//
-//  Created by Michael on 2013-10-20.
-//  Copyright (c) 2013 Michael. All rights reserved.
-//
-
 #import <XCTest/XCTest.h>
+#import "DGGood.h"
+#import "DGCategory.h"
+#import "DGUser.h"
 
-@interface DoGoodTests : XCTestCase
+@interface DGGoodTests : XCTestCase {
+    DGGood *good;
+}
 
 @end
 
-@implementation DoGoodTests
+@implementation DGGoodTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    good = [[DGGood alloc] init];
+    good.caption = @"Hey";
+    good.goodID = [NSNumber numberWithInt:1];
+    good.evidence = @"http://images.dogood.com";
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown {
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTAssertTrue(true);
+- (void)testGoodExists {
+    XCTAssertNotNil(good, @"good must exist");
+}
+
+- (void)testGoodHasACaption {
+    XCTAssertEqual(good.caption, @"Hey", @"caption must be set");
+}
+
+- (void)testGoodHasAnID {
+    XCTAssertEqual(good.goodID, [NSNumber numberWithInt:1], @"good ID must be set");
+}
+
+- (void)testGoodHasEvidence {
+    NSURL *url = [NSURL URLWithString:good.evidence];
+    XCTAssertEqualObjects([url absoluteString],
+                          @"http://images.dogood.com",
+                         @"The Good's evidence should be represented by a URL");
+}
+
+- (void)testGoodHasACategory {
+    DGCategory *category = [[DGCategory alloc] init];
+    good.category = category;
+    XCTAssertEqual(good.category, category, @"good must have a category");
+}
+
+- (void)testGoodBelongsToAUser {
+    DGUser *user = [[DGUser alloc] init];
+    good.user = user;
+    XCTAssertEqual(good.user, user, @"good must have a user");
 }
 
 @end
