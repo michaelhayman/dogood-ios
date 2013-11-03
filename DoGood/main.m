329ea5b6d41a3e16b6cf284ba109416311f00897
Michael Hayman <michael.hayman@zoocasa.com>
2013-11-03 14:03:42 -0500

    Remove NUI references.

--- DoGood/main.m
+++ DoGood/main.m
@@ -1,11 +1,8 @@
 #import "DGAppDelegate.h"
-// #import <NUI/NUISettings.h>
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        // [NUISettings initWithStylesheet:@"DoGood"];
-        // [NUISettings setAutoUpdatePath:@"/Users/mhayman/setup/springbox/DoGood/DoGood/DoGood/DoGood.nss"];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([DGAppDelegate class]));
     }
 }
