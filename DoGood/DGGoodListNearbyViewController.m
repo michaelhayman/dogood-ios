290ee239c38ba49a251765963919e79eca38473f
Michael Hayman <michael@springbox.ca>
2014-04-03 06:43:26 -0400

    Stop updating location when view disappears.

--- DoGood/DGGoodListNearbyViewController.m
+++ DoGood/DGGoodListNearbyViewController.m
@@ -31,6 +31,7 @@
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     [ProgressHUD dismiss];
+    [locationManager stopUpdatingLocation];
 }
 
 - (void)didReceiveMemoryWarning {
