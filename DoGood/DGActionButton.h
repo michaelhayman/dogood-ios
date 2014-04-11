8b87462bbbf53f1fecdb6ab315a1fca760818876
Michael Hayman <michael@springbox.ca>
2014-04-11 01:24:41 -0400

    Experiment with UIButton subclass.

---
+++ DoGood/DGActionButton.h
@@ -0,0 +1,13 @@
+//
+//  DGActionButton.h
+//  DoGood
+//
+//  Created by Michael on 2014-04-08.
+//  Copyright (c) 2014 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGActionButton : UIButton
+
+@end
---
+++ DoGood/DGActionButton.m
@@ -0,0 +1,26 @@
+//
+//  DGActionButton.m
+//  DoGood
+//
+//  Created by Michael on 2014-04-08.
+//  Copyright (c) 2014 Michael. All rights reserved.
+//
+
+#import "DGActionButton.h"
+
+@implementation DGActionButton
+
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+
+    if (self) {
+        UIImage *on = [[UIImage imageNamed:@"bt_lrg_off"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 11, 0, 6)];
+        UIImage *off = [[UIImage imageNamed:@"bt_lrg_on"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 11, 0, 6)];
+        [self setBackgroundImage:on forState:UIControlStateNormal];
+        [self setBackgroundImage:off forState:UIControlStateHighlighted];
+        self.titleLabel.font = [UIFont boldSystemFontOfSize:12];
+    }
+    return self;
+}
+
+@end
