ea9aeac4d141e1c5b83665190dd563fe9db0dd62
Michael Hayman <michael@springbox.ca>
2014-04-11 04:24:37 -0400

    Use sketch post icon on home screen.

--- DoGood/DGExploreViewController.m
+++ DoGood/DGExploreViewController.m
@@ -44,6 +44,9 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self showWelcome];
+    NSString *menuButton = @"Post";
+    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:menuButton] style:UIBarButtonItemStylePlain target:self action:@selector(postGood:)];
+    self.navigationItem.rightBarButtonItem = rightButton;
 }
 
 - (IBAction)postGood:(id)sender {
