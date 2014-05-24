fb999ebd663bdf16c2bc03e7f786471a690b16e2
Michael Hayman <michael@springbox.ca>
2014-05-23 20:36:52 -0400

    Add custom back icon.

--- DoGood/DGViewController.m
+++ DoGood/DGViewController.m
@@ -9,6 +9,9 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     self.navigationItem.backBarButtonItem = [DGAppearance barButtonItemWithNoText];
+    [UINavigationBar appearance].backIndicatorImage = [UIImage imageNamed:@"BackButton"];
+    [UINavigationBar appearance].backIndicatorTransitionMaskImage = [UIImage imageNamed:@"BackButton"];
+    [UINavigationBar appearance].tintColor = [UIColor whiteColor];
     UINavigationBar *navigationBar = self.navigationController.navigationBar;
     [navigationBar hideBottomHairline];
 }
