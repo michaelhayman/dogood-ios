69fd8b747a882db82344747154e51cd54af0903c
Michael Hayman <michael@springbox.ca>
2014-05-28 08:46:31 -0400

    Call super on viewWillAppear.

--- DoGood/DGSignUpDetailsViewController.m
+++ DoGood/DGSignUpDetailsViewController.m
@@ -36,10 +36,12 @@
 }
 
 - (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:NO];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:NO];
 }
 
