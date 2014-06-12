9941c30727157a336f5c950d9f029bb73c37dfe0
Michael Hayman <michael@springbox.ca>
2014-06-12 18:43:43 -0400

    Add loading & no results view to user list.

--- DoGood/DGUserListViewController.h
+++ DoGood/DGUserListViewController.h
@@ -1,6 +1,10 @@
+@class SAMLoadingView;
+
 @interface DGUserListViewController : DGViewController <UITableViewDelegate, UITableViewDataSource> {
     __weak IBOutlet UITableView *tableView;
+    BOOL showNoResultsMessage;
     NSArray *users;
+    SAMLoadingView *loadingView;
 }
 
 @property (retain, nonatomic) NSString *type;
--- DoGood/DGUserListViewController.m
+++ DoGood/DGUserListViewController.m
@@ -1,5 +1,7 @@
 #import "DGUserListViewController.h"
 #import "UserCell.h"
+#import "NoResultsCell.h"
+#import <SAMLoadingView/SAMLoadingView.h>
 
 @interface DGUserListViewController ()
 
@@ -14,12 +16,19 @@
 
     UINib *nib = [UINib nibWithNibName:@"UserCell" bundle:nil];
     [tableView registerNib:nib forCellReuseIdentifier:@"UserCell"];
+    UINib *noResultsNib = [UINib nibWithNibName:kNoResultsCell bundle:nil];
+    [tableView registerNib:noResultsNib forCellReuseIdentifier:kNoResultsCell];
 
     UIRefreshControl *refreshControl = [UIRefreshControl new];
     refreshControl.tintColor = VIVID;
 
     [refreshControl addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
     [tableView addSubview:refreshControl];
+
+    showNoResultsMessage = NO;
+
+    loadingView = [[SAMLoadingView alloc] initWithFrame:tableView.bounds];
+    loadingView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
     /*
     if ([self.query isEqualToString:@"following"]) {
@@ -48,26 +57,47 @@
 
 #pragma mark - UITableView delegate methods
 - (UITableViewCell *)tableView:(UITableView *)aTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString * reuseIdentifier = @"UserCell";
-    UserCell *cell = [aTableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
-    DGUser *user = users[indexPath.row];
-    cell.user = user;
-    DebugLog(@"user %@", user);
-    [cell setValues];
-    cell.navigationController = self.navigationController;
-    return cell;
+    if (indexPath.section == 0) {
+        static NSString * reuseIdentifier = @"UserCell";
+        UserCell *cell = [aTableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
+        DGUser *user = users[indexPath.row];
+        cell.user = user;
+        DebugLog(@"user %@", user);
+        [cell setValues];
+        cell.navigationController = self.navigationController;
+        return cell;
+    } else {
+        static NSString * reuseIdentifier = kNoResultsCell;
+        NoResultsCell *cell = [aTableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
+        [cell setHeading:@"No users found" explanation:@"" andImage:[UIImage imageNamed:@"NoPeople"]];
+        return cell;
+    }
 }
 
 - (CGFloat)tableView:(UITableView *)aTableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 60;
+    if (indexPath.section == 0) {
+        return 60;
+    } else {
+        return kNoResultsCellHeight;
+    }
 }
 
 - (NSInteger)tableView:(UITableView *)tblView numberOfRowsInSection:(NSInteger)section {
-    return [users count];
+    if (section == 0) {
+        return [users count];
+    } else {
+        if (showNoResultsMessage == YES) {
+            tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+            return 1;
+        } else {
+            tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+            return 0;
+        }
+    }
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return 2;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
@@ -76,13 +106,21 @@
 
 #pragma mark - Retrieval methods
 - (void)getUsers {
+    [tableView addSubview:loadingView];
+
     [[RKObjectManager sharedManager] getObjectsAtPath:[NSString stringWithFormat:@"/users/%@?type=%@&id=%@", self.query, self.type, self.typeID] parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
         users = [[NSArray alloc] initWithArray:mappingResult.array];
+        if ([users count] == 0)  {
+            showNoResultsMessage = YES;
+        } else {
+            showNoResultsMessage = NO;
+        }
         [tableView reloadData];
+        [loadingView removeFromSuperview];
     } failure:^(RKObjectRequestOperation *operation, NSError *error) {
         DebugLog(@"Operation failed with error: %@", error);
+        [loadingView removeFromSuperview];
     }];
 }
 
 @end
-
