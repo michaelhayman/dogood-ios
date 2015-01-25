87e24fcdf81849f7f5f9a88ba1aa535764ce20cf
Michael Hayman <michael@springbox.ca>
2015-01-25 09:32:20 -0500

    Rename RestKit tests.

---
+++ DoGoodTests/DGRestKitTests.m
@@ -0,0 +1,27 @@
+//
+//  DGRestKitTest.m
+//  DoGood
+//
+//  Created by Michael on 2013-10-21.
+//  Copyright (c) 2013 Michael. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <RestKit/RestKit.h>
+// #import <RestKit/Testing.h>
+
+@interface DGRestKitTests : XCTestCase
+
+@end
+
+@implementation DGRestKitTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
